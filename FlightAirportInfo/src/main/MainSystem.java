f1366b1b9c74ff87bdde607176c7d28edd00e67b
Florian Le Pallec <flepall@a-127579.ups.u-psud.fr>
2017-06-02 12:31:54 +0200

    aa

---
+++ FlightAirportInfo/src/main/MainSystem.java
@@ -0,0 +1,15 @@
+package main;
+
+import java.util.ArrayList;
+
+public class MainSystem 
+{
+	private ArrayList<Airport> listAirports;
+	private ArrayList<Flight> listFlight;
+	
+	public MainSystem()
+	{
+		listAirports = new ArrayList<Airport>();
+		listAirports = new ArrayList<Airport>();
+	}
+}
