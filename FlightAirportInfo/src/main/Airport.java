54949c6682a1e5397b6c2ef5b72d0da4a8eca6c5
Vincent Doisneau <vincent.doisneau@a-127580.ups.u-psud.fr>
2017-06-02 14:39:23 +0200

    Changements airport

--- FlightAirportInfo/src/main/Airport.java
+++ FlightAirportInfo/src/main/Airport.java
@@ -3,6 +3,8 @@ package main;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Airport 
 {
@@ -32,10 +34,55 @@ public class Airport
 			while(line != null)
 			{
 				String[] array = line.split(",");
-				int id = Integer.parseInt(array[0]);
-				float val = Float.parseFloat(array[0]);
+				char[] res;
+				array[0].getChars(0,6,res,0);
+				System.out.println(res);
+				ArrayList <String> villeA = new ArrayList();
+				ArrayList <String> paysA = new ArrayList();
+				ArrayList <String> codeA = new ArrayList();
+				ArrayList <String> latitudeA = new ArrayList();
+				ArrayList <String> longitudeA = new ArrayList();
+				int i=0;
+				while(!(array[i].equals("/"))){
+					villeA.add(array[i]);
+					i++;
+				}
+				while(array[i].equals("/")){
+					i++;
+				}
+				while(!array[i].equals("/")){
+					paysA.add(array[i]);
+					i++;
+				}
+				while(array[i].equals("/")){
+					i++;
+				}
+				while(!array[i].equals("/")){
+					codeA.add(array[i]);
+					i++;
+				}
+				while(array[i].equals("/")){
+					i++;
+				}
+				while(!array[i].equals("/")){
+					latitudeA.add(array[i]);
+					i++;
+				}
+				while(array[i].equals("/")){
+					i++;
+				}
+				while(!array[i].equals("/")){
+					longitudeA.add(array[i]);
+					i++;
+				}
+				Iterator <String> it = villeA.iterator();
+				while(it.hasNext()){
+					System.out.println(it.next());
+				}
+				
+				//int id = Integer.parseInt(array[0]);
+				//float val = Float.parseFloat(array[0]);
 				line = bufRead.readLine();
-				System.out.println(line);
 			}
 			bufRead.close();
 			file.close();
